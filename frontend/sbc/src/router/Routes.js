f761c62708a90d33ec24bb93054c9e14353102f0
danielwalke <daniel.walke@ovgu.de>
2024-03-08 11:47:37 +0100

    disclaimer and impressum

--- frontend/sbc/src/router/Routes.js
+++ frontend/sbc/src/router/Routes.js
@@ -1,9 +1,11 @@
 import TableInput from "../components/TableInput.vue";
 import DetailContent from "../components/details/DetailContent.vue";
 import About from "../components/about/About.vue";
+import Disclaimer from "../components/disclaimer/Disclaimer.vue";
 
 export const routes = [
-	{ path: '/', component: TableInput },
+	{ path: '/sbc_frontend', component: TableInput },
 	{ path: '/about', component: About },
+	{ path: '/disclaimer', component: Disclaimer },
 	{ path: '/details/:id', component: DetailContent}
 ]
