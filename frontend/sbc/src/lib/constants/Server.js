81bde2c756f5f2170aba2821e4e60c754be014b9
danielwalke <daniel.walke@ovgu.de>
2024-04-02 07:39:00 +0200

    refactored some code

--- frontend/sbc/src/lib/constants/Server.js
+++ frontend/sbc/src/lib/constants/Server.js
@@ -1,4 +1,9 @@
-export const SERVER_URL = "http://localhost:8000/"
+// export const SERVER_URL = "http://localhost:8000/sbc/"
+export const SERVER_URL = "http://141.44.141.132/sbc/"
+// export const SERVER_URL = "https://prophane.de/sbc/"
+// export const SERVER_URL = "https://prophane.de/sbc/"
+// export const SERVER_URL = "https://mdoa-tools.bi.denbi.de/sbc/"
+
 
 export const PORT = 8000
 
