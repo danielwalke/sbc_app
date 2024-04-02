349aa6662b692ab5caeb3e3f285a0758c38cb02b
danielwalke <daniel.walke@ovgu.de>
2024-04-02 11:31:54 +0200

    minor responsive changes

--- frontend/sbc/src/components/input/FileInput.vue
+++ frontend/sbc/src/components/input/FileInput.vue
@@ -1,8 +1,8 @@
 <template>
-      <label for="file-input" class="rounded-md shadow-md hover:scale-105 p-4 bg-sky-700 cursor-pointer hover:bg-sky-600">
-        File Upload
-      </label>
-      <input class="hidden" type="file" id="file-input" multiple @change="onFileInputChange" />
+	<label for="file-input" class="rounded-md shadow-md hover:scale-105 p-4 bg-sky-700 cursor-pointer hover:bg-sky-600">
+		File Upload
+	</label>
+	<input class="hidden" type="file" id="file-input" multiple @change="onFileInputChange" />
 </template>
 
 <script setup>
