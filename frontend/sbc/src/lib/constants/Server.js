06e7ad264a0779c406631b3c2d8229131ed9ea33
danielwalke <daniel.walke@ovgu.de>
2024-04-17 09:48:03 +0200

    changed predictions

--- frontend/sbc/src/lib/constants/Server.js
+++ frontend/sbc/src/lib/constants/Server.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = "http://141.44.141.132/sbc/"
-// export const SERVER_URL = "http://localhost:8000/sbc/"
+// export const SERVER_URL = "http://141.44.141.132/sbc/"
+export const SERVER_URL = "http://localhost:8000/sbc/"
 // export const SERVER_URL = "https://mdoa-tools.bi.denbi.de/sbc/"
 
 
