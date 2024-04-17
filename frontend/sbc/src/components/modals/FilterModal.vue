3528fda061ce4f37ab0ae03dcd7645251c526efa
danielwalke <daniel.walke@ovgu.de>
2024-04-17 10:34:14 +0200

    fixed bug in filetr

--- frontend/sbc/src/components/modals/FilterModal.vue
+++ frontend/sbc/src/components/modals/FilterModal.vue
@@ -10,10 +10,7 @@
 						{{headerContent}}
 					</h3>
 					<button @click="close" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-						<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-							<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-						</svg>
-						<span class="sr-only">Close modal</span>
+						X
 					</button>
 				</div>
 				<!-- Modal body -->
@@ -125,6 +122,7 @@ function close(){
 }
 
 watch(selectedValue, (newSelectedValue) => {
+
 	store.setFilters(store.getFilters.filter(filter => filter["filterKey"] !== store.getFilterKey))
 	if(newSelectedValue !== undefined) {
 		store.addFilter({
@@ -137,10 +135,19 @@ watch(selectedValue, (newSelectedValue) => {
 })
 
 watch(filterKey, (newFilterKey) => {
-	const filter = store.getFilters.find(filter => filter["filterKey"] !== newFilterKey.value)
-	if(filter === undefined) return
+
+	const filter = store.getFilters.find(filter => filter["filterKey"] === store.getFilterKey)
+	if(filter === undefined) {
+		selectedValue.value = undefined
+		return
+	}
 	selectedValue.value = filter.selectedValue
 
+})
+
+onUpdated(()=>{
+	console.log("updated")
+	console.log(store.getFilters)
 })
 
 </script>
