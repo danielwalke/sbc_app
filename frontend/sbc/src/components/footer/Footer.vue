98807ca994345af8b51d7f1e59935887767bee06
danielwalke <daniel-walke@t-online.de>
2025-03-27 11:14:38 +0100

    corrected footer

--- frontend/sbc/src/components/footer/Footer.vue
+++ frontend/sbc/src/components/footer/Footer.vue
@@ -1,13 +1,13 @@
 <template>
-<div class="h-12 text-black flex items-center justify-center bg-gray-700">
-	<div class="flex gap-8 overflow-hidden text-xs md:text-sm lg:text-base">
-		<router-link to="/sbc-shap" class="route">Main-App</router-link>
-    <router-link to="/yt-tutorials" class="route">YT-Tutorials</router-link>
-		<router-link to="/sbc-shap/disclaimer" class="route">Disclaimer</router-link>
-		<router-link to="/sbc-shap/about" class="route">Impressum</router-link>
-    <router-link to="/sbc-shap/data-privacy" class="route">Data privacy</router-link>
-	</div>
-</div>
+  <div class="h-12 text-black flex items-center justify-center bg-gray-700 w-full overflow-x-auto">
+    <div class="pl-32 flex gap-4 lg:gap-8 text-xs md:text-sm lg:text-base min-w-max">
+      <router-link to="/sbc-shap" class="route">Main-App</router-link>
+      <router-link to="/yt-tutorials" class="route">YT-Tutorials</router-link>
+      <router-link to="/sbc-shap/disclaimer" class="route">Disclaimer</router-link>
+      <router-link to="/sbc-shap/about" class="route">Impressum</router-link>
+      <router-link to="/sbc-shap/data-privacy" class="route">Data privacy</router-link>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
