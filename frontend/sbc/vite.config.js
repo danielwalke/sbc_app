d058517a36497dc14a6993b4ae839b1047e95663
danielwalke <daniel-walke@t-online.de>
2025-03-28 06:53:34 +0100

    updated config and docker

--- frontend/sbc/vite.config.js
+++ frontend/sbc/vite.config.js
@@ -9,7 +9,13 @@ export default defineConfig({
     host: true,
     port: 4173,
     preview: {
-      allowedHosts: true
-    }
+      allowedHosts: ["daniel-walke.com"]
+    },
+    cors: {
+      origin: ['daniel-walke.com', 'http:localhost:4173', 'http://localhost:5173', "https://mdoa-tools.bi.denbi.de/", "mdoa-tools.bi.denbi.de"],
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type']
+    },
+    allowedHosts: ['daniel-walke.com', "https://mdoa-tools.bi.denbi.de/", "mdoa-tools.bi.denbi.de"]
   }
 })
