616010f63dbad84829ba9905d6ea81632a8a15d0
danielwalke <daniel-walke@t-online.de>
2025-04-09 10:55:49 +0200

    updated server

--- frontend/sbc/src/lib/constants/Server.js
+++ frontend/sbc/src/lib/constants/Server.js
@@ -2,8 +2,8 @@
 // export const SERVER_URL = "http://141.44.141.132/sbc/"
 // export const SERVER_URL = "https://prophane.de/sbc/"
 // export const SERVER_URL = "https://prophane.de/sbc/"
-export const SERVER_URL = "https://mdoa-tools.bi.denbi.de/sbc_backend/"
-// export const SERVER_URL = "https://daniel-walke.com/sbc_backend/"
+// export const SERVER_URL = "https://mdoa-tools.bi.denbi.de/sbc_backend/"
+export const SERVER_URL = "https://daniel-walke.com/sbc_backend/"
 
 
 export const PORT = 8000
